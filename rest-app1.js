c1b6721dd3e18b1d2ddcaace8aacc00d314daee1
Vinod Santhanam <yes_vinod@yahoo.com>
2018-05-23 10:30:21 -0400

    added another sample of rest api

--- rest-app1.js
+++ rest-app1.js
@@ -1,4 +1,5 @@
 const http = require('http');
+
 var MongoClient = require('mongodb').MongoClient;
 var configItems = require('./config');
 const express = require('express');
@@ -27,71 +28,53 @@ app.post('/insert-one-publisher', (req, res) => {
     var response = new Array();
     MongoClient.connect(uri, function (err, client) {
         console.log("Request body " + JSON.stringify(req.body));
-   
-
-        var publisherDetails= req.body;
+        var publisherDetails = req.body;
         console.log(publisherDetails.name);
-        
+
         const books_collection = client.db("books-db").collection("publisher");
-           books_collection.insertOne(publisherDetails, function (err, dbres) {
-                if (err) {
-                    console.log(err);
-                    res.json(err);
-                } else {
-                    console.log("1 document inserted" + res.insertedCount);
-                    res.json(dbres.insertedCount);
-                }
-                client.close();
-    
-            });
+        books_collection.insertOne(publisherDetails, function (err, dbres) {
+            if (err) {
+                console.log(err);
+                res.json(err);
+            } else {
+                console.log("1 document inserted" + res.insertedCount);
+                res.json(dbres.insertedCount);
+            }
+            client.close();
+        });
 
     })
 });
-
-
-//Promise Method Implementation:
-function callDatabasePromise(){
-
-    return new Promise(function(resolve,reject){
-        
-    })
-}
-
-
 
 
 app.get('/all-publishers', (req, res) => {
     //Insert Publisher Data to Mongo DB
     var uri = configItems.storageConfig.mongo_uri;
     console.log(uri);
-    var promise = new callDatabasePromise(function(result){
-
-    },function(err){
-
-    });
     var response = new Array();
     MongoClient.connect(uri, function (err, client) {
         const collection = client.db("books-db").collection("publisher");
-        var cursor = collection.find();
-        var counter = 0;
-        var findRecordCount = cursor.count(function (err, count) {
-            length = count;
-            console.log("find count >>" + count);
-        });
-        cursor.forEach(function (doc, err) {
-
-            response.push(doc);
-            if (err) console.log(err);
-            counter = counter + 1;
-            console.log("Counter Value " + counter);
-            console.log("Array Lenght " + length);
-            if (counter == length) {
-                sendResponse(response);
+        var cursor = collection.find({}, function (err, resultCursor) {
+            function processItem(err, item) {
+                if (item === null) {
+                    console.log('all done');
+                    client.close();
+                    return res.json(response);
+                }
+                console.log(item);
+                response.push(item);
+                resultCursor.next(processItem);
             }
-        })
+            resultCursor.next(processItem);
+
+        });
+
+
+
+
         // console.log(collection);
         // perform actions on the collection object
-        client.close();
+
     });
 
     function sendResponse(local_response) {
